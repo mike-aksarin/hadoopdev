06019d34ed116cbb27d7ca951858d30ccec4a1e7
mike aksarin <mike.aksarin@gmail.com>
2019-05-08 16:50:49 +0300

    yum sbt shell-script for cloudera-quickstart docker image

--- install-sbt.sh
+++ install-sbt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# docker cp bintray--sbt-rpm.repo cdh:etc/yum.repos.d
+# docker cp conf/bintray--sbt-rpm.repo cdq:etc/yum.repos.d
 
 yum install java-1.8.0-openjdk
 yum -y install ca-certificates openssl nss
